0d11ee5dfd9c3dba7b2ffd40a77dda6e83a08a47
sonamjain2810 <sonamjain2810@yahoo.com>
2025-04-29 20:04:00 +0530

    Added player respawn system after death
    - Players now respawn at random spawn points after dying
    - Respawn delay and health reset handled via coroutine
    - Renderer and collider toggled to simulate death state
    - Clean and conflict-free with existing movement setup

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,6 +1,7 @@
 using Photon.Pun;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 
 public class NetworkPlayer : MonoBehaviourPun
@@ -13,6 +14,10 @@ public class NetworkPlayer : MonoBehaviourPun
 
     private Renderer playerRenderer;
     public Transform shootPoint;
+
+    public float respawnDelay = 3f;
+    private Vector3[] spawnPositions;
+
 
     void Awake()
     {
@@ -29,6 +34,12 @@ public class NetworkPlayer : MonoBehaviourPun
 
             Color randomColor = Random.ColorHSV(0f, 1f, 0.8f, 1f, 0.8f, 1f); // Vibrant random color
             photonView.RPC("SetColor", RpcTarget.AllBuffered, randomColor.r, randomColor.g, randomColor.b);
+
+            Transform[] spawnPoints = GameObject.Find("SpawnPoints").GetComponentsInChildren<Transform>();
+            spawnPositions = new Vector3[spawnPoints.Length - 1];
+
+            for (int i = 1; i < spawnPoints.Length; i++)
+                spawnPositions[i - 1] = spawnPoints[i].position;
 
         }
         nicknameText.text = photonView.Owner.NickName;
@@ -71,7 +82,9 @@ public class NetworkPlayer : MonoBehaviourPun
 
         if (currentHealth <= 0)
         {
-            Die();
+                    photonView.RPC("DieAndRespawn", RpcTarget.AllBuffered);
+
+            
         }
     }
 
@@ -86,8 +99,31 @@ void SetColor(float r, float g, float b)
 }
 
 
-    void Die()
+   [PunRPC]
+void DieAndRespawn()
+{
+    if (photonView.IsMine)
+        StartCoroutine(RespawnRoutine());
+}
+
+    IEnumerator RespawnRoutine()
     {
-        PhotonNetwork.Destroy(gameObject);
+        // Disable player temporarily
+        GetComponent<Collider>().enabled = false;
+        GetComponentInChildren<Renderer>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(respawnDelay);
+
+        // Move to new spawn position
+        Vector3 respawnPos = spawnPositions[Random.Range(0, spawnPositions.Length)];
+        transform.position = respawnPos;
+
+        // Restore player state
+        currentHealth = maxHealth;
+        GetComponent<Collider>().enabled = true;
+        GetComponentInChildren<Renderer>().enabled = true;
+        this.enabled = true;
     }
+
 }
